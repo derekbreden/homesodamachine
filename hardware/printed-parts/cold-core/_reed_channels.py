7b89f6b6fcb4656ad3ad6ed24c53d4b477533201
Derek Bredensteiner <derekbreden@gmail.com>
2026-05-17 01:42:50 -0500

    _reed_channels: adopt flip_z in make_box and inner_fillet_cut

--- hardware/printed-parts/cold-core/_reed_channels.py
+++ hardware/printed-parts/cold-core/_reed_channels.py
@@ -6,6 +6,7 @@ import cadquery as cq
 from _cold_core_interface import (
     xz_plane_y_up,
     xy_plane_z_up,
+    flip_z,
     wall_and_floor_thickness,
     foam_shell_outer_height,
     bag_pocket_width,
@@ -66,7 +67,7 @@ def make_box(x_range, y_range, z_range):
     return (
         cq.Workplane(xz_plane_y_up)
         .workplane(offset=y_min)
-        .moveTo((x_min + x_max) / 2, -(z_min + z_max) / 2)
+        .moveTo(*flip_z(((x_min + x_max) / 2, (z_min + z_max) / 2)))
         .rect(x_max - x_min, z_max - z_min)
         .extrude(y_max - y_min)
     )
@@ -136,7 +137,7 @@ def build_reed_channels(side):
     inner_fillet_cut = (
         cq.Workplane(xz_plane_y_up)
         .workplane(offset=cable_envelope_wedge_apex_y)
-        .moveTo(fillet_axis_x, -fillet_axis_z)
+        .moveTo(*flip_z((fillet_axis_x, fillet_axis_z)))
         .circle(outer_corner_r)
         .extrude(outer_corner_r)
     )
